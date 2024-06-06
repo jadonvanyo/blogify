62be8ae2b2bb33bb4573a744a4cbe0b6c2957102
jayman779 <jadon.vanyo@gmail.com>
2024-06-06 19:08:25 -0400

    feat: connected the postgresql database

--- backend/ai_blog_app/ai_blog_app/settings.py
+++ backend/ai_blog_app/ai_blog_app/settings.py
@@ -73,10 +73,21 @@ WSGI_APPLICATION = 'ai_blog_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blogify',
+        'USER': 'superuser',
+        'PASSWORD': 'password',
+        'HOST': 'blogify.cl8s8kso6kaa.us-east-2.rds.amazonaws.com',
+        'PORT': '5432'
     }
 }
 
