ccd2c2729312a833e53a61255ddca651c32d04fd
jayman779 <jadon.vanyo@gmail.com>
2024-06-06 19:07:32 -0400

    feat: added signup and login for the app

--- backend/ai_blog_app/blog_generator/views.py
+++ backend/ai_blog_app/blog_generator/views.py
@@ -1,5 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, login
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def user_login(request):
+    return render(request, 'login.html')
+
+def user_signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        repeatPassword = request.POST['repeatPassword']
+        
+        # Determine if password and repeat password do not match
+        if password != repeatPassword:
+            error_message = 'Password does not match'
+            return render(request, 'signup.html', {'error_message':error_message})
+        # Try to create a user
+        try: 
+            user = User.objects.create_user(username, email, password)
+            user.save()
+            login(request, user)
+            return redirect('/')
+        except:
+            error_message = 'Error creating account'
+            return render(request, 'signup.html', {'error_message':error_message})
+    
+    # Return the signup page for get request
+    return render(request, 'signup.html')
+
+def user_logout(request):
+    pass
