4cff676af279aa9bca90ddb236b613adcddd140b
jayman779 <jadon.vanyo@gmail.com>
2024-06-07 10:14:25 -0400

    feat: added redirect to login page if user not logged in

--- backend/ai_blog_app/ai_blog_app/settings.py
+++ backend/ai_blog_app/ai_blog_app/settings.py
@@ -132,3 +132,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Default url to redirect user not logged in
+
+LOGIN_URL = 'login'
